01f019a4ab7d17f1b3dae4ef1fa747526e12453e
Raven <raven.liu@softbi.com>
2023-10-06 16:38:29 +0800

    test variable

--- variable.tf
+++ variable.tf
@@ -1,9 +1,3 @@
-variable "GCP_PROJECT" {
-  description = "GCP Project ID"
-  type        = string
-  default     = "peppy-ward-398202"
-}
-
 variable "gcr_name" {
   type    = string
   default = "cathay-start-gcr-module"
